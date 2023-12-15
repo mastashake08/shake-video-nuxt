2f4a5faf4235776a54e8c8bba0e41d2097e881af
Hacker God <jyrone.parker@gmail.com>
2023-12-15 02:01:20 -0500

    still config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,17 +1,5 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: [
-    '@invictus.codes/nuxt-vuetify',
-    '@vite-pwa/nuxt',
-    '@nuxtjs/color-mode'
-  ],
-  runtimeConfig: {
-    app:{
-    config: {
-      compilerOptions: {
-        isCustomElement: 'shake-video-element'
-      }
-    }
-  }}
+  modules: ['@invictus.codes/nuxt-vuetify']
 })
