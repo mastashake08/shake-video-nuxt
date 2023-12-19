5110b2b4cfafb328e8dda2ca629e6f785d060854
Jyrone Parker <jyrone.parker@atlasied.com>
2023-12-18 20:30:39 -0500

    playlist

--- components/VideoPlayer.vue
+++ components/VideoPlayer.vue
@@ -1,12 +1,15 @@
 <template>
     <div>
       <video ref="videoPlayer" class="video video-js"></video>
+      <div class="vjs-playlist"></div>
     </div>
   </template>
   
   <script>
 import videojs from 'video.js';
+import videojsPlaylistPlugin from 'videojs-playlist';
 import 'video.js/dist/video-js.css';
+import 'videojs-playlist/dist/videojs-playlist-ui.css'
   
   export default {
     name: 'VideoPlayer',
@@ -24,9 +27,12 @@ import 'video.js/dist/video-js.css';
       }
     },
     mounted() {
+
+      videojs.registerPlugin('playlist', videojsPlaylistPlugin);
       this.player = videojs(this.$refs.videoPlayer, this.options, () => {
         this.player.log('onPlayerReady', this);
       });
+      console.log('PLAYLIST', this.player.playlist)
     },
     beforeDestroy() {
       if (this.player) {
